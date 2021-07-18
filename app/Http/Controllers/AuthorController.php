e72660f17caeffd78cc77c33886e0dfe8c9bf3de
M Arya Dyas <aryadotid21@gmail.com>
2021-07-18 15:21:26 +0700

    Beautify JSON Data

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -9,21 +9,36 @@ class AuthorController extends Controller
     
     public function showAllAuthors()
     {
-        return response()->json(Author::all());
+        $data = Author::all();
+        if($data->isNotEmpty()){
+            return response()->json([
+                'success'=> true, 
+                'data' => Author::all()
+            ]);
+        }
+        else {
+            return response()->json(array(
+                'success'=>false, 
+                'message' => 'Not Found!'
+            ),500);
+        }  
     }
-
     public function showOneAuthor($id)
     {
         $data = Author::find($id);
-        if(!$data==null)
-            return response()->json($data);
-        else
+        if(!$data==null){
+            return response()->json([  
+                'success'=>true, 
+                'data' => $data,
+            ]);
+        }
+        else{
             return response()->json(array(
-                'status' => 'error',
+                'success'=>false, 
                 'message' => 'Not Found!'
             ),500);
+        }
     }
-
     public function create(Request $request)
     {
         $this->validate($request, [
@@ -31,23 +46,39 @@ class AuthorController extends Controller
             'email' => 'required|email|unique:authors',
             'location' => 'required|alpha'
         ]);
-        
         $author = Author::create($request->all());
-
-        return response()->json($author, 201);
+        return response()->json([  
+            'success'=>true, 
+            'data' => $author,
+        ],201);
     }
-
     public function update($id, Request $request)
     {
+        $this->validate($request, [
+        'name' => 'required',
+        'email' => 'required|email|unique:authors',
+        'location' => 'required|alpha'
+        ]);
         $author = Author::findOrFail($id);
         $author->update($request->all());
-
-        return response()->json($author, 200);
+            return response()->json([
+                'success'=>true, 
+                'data' => $author
+            ], 200);
     }
-
     public function delete($id)
     {
-        Author::findOrFail($id)->delete();
-        return response('Deleted Successfully', 200);
+        $data = Author::destroy($id);
+        if($data){
+            return response()->json([
+                'success'=>true, 
+                'message' => 'Deleted Succesfully'
+            ], 200);
+    } else {
+        return response()->json([
+            'success'=>false, 
+            'message'=>'User not found!',
+        ], 500);
+    }
     }
 }
